6de65da7753dc7160c2a352eac4d0099a0c4b6c2
liyamcode <liyampatel123@gmail.com>
2025-03-30 17:07:00 -0400

    updated

--- Java/Compare.java
+++ Java/Compare.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 
-class Main {
+class Compare { // Fixed class name to match the filename
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
         String word1, word2;
         
-        //Asks user for their first word
+        // Asks user for their first word
         System.out.print("What is your first word: ");
         word1 = input.nextLine();
-        //Asks user for their second word
+        // Asks user for their second word
         System.out.print("What is your second word: ");
         word2 = input.nextLine();
         
-        //will check if both words are equal
-        if (word1.equalsIgnoreCase(word2)){
-            //if words are equal it will print true
+        // Will check if both words are equal
+        if (word1.equalsIgnoreCase(word2)) {
+            // If words are equal it will print true
             System.out.print("True");
         } else {
-            //if words are not equal it will print false
+            // If words are not equal it will print false
             System.out.print("False");
         }
+        input.close(); // Added to close the Scanner
     }
 }
